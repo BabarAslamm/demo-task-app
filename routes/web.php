5f6e50e86cdf999938fc80d5d4281b312bc68bcc
mac <babaraslam826@gmail.com>
2025-04-14 17:56:57 +0500

    FeeCalculatorPage on / url

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,14 @@ Route::get('/fee-calculator', function () {
 });
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+
+    return Inertia::render('FeeCalculatorPage');
+    // return Inertia::render('Welcome', [
+    //     'canLogin' => Route::has('login'),
+    //     'canRegister' => Route::has('register'),
+    //     'laravelVersion' => Application::VERSION,
+    //     'phpVersion' => PHP_VERSION,
+    // ]);
 });
 
 Route::get('/dashboard', function () {
